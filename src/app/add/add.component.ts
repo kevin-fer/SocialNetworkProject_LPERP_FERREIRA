34fa89468b7da088d886834f0d6882fd7b653a4a
kevin-fer <pro.kevinferreira@gmail.com>
2021-02-18 12:37:50 +0100

    fin de séance

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  title = new FormControl('');
+
+  addPost (): void {
+
+    
+  }
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
